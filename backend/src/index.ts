dfb9664087ca130072fe204733ca7f0c7abc8055
Aryan Kumar <aryanshu919@gmail.com>
2024-07-22 12:48:43 +0530

    added middlewares in blog route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings:{
@@ -10,75 +10,99 @@ const app = new Hono<{
   },
 }>()
 
-app.post('/api/v1/signin', async (c) =>{
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
+//--------- MIDDLEWARES ----------
 
-  const body = await c.req.json();
-  console.log("creating user")
-  const user =  await prisma.user.create({
-    data:{
-      email : body.email,
-      password : body.password
-    }
-  });
+app.use('/api/v1/blog/*', async (c,next) =>{
+   
+  const header = c.req.header('authorization') || ""
 
-  const payload ={
-    id: user.id,
-    exp: Math.floor(Date.now() / 1000) + 60 * 5, 
+  const token = header.split(' ')[1]
+  const respone = await verify(token, c.env.JWT_SECRET)
+
+  if(respone.id){
+    next();
   }
-
-  const token = await sign(payload,c.env.JWT_SECRET)
- 
-
   return c.json({
-    jwt : token
+    status : 403,
+    msg : "unauthorized"
   })
+})
+
+
+//---------- ROUTES --------------
+
+app.post('/api/v1/signin', async (c) =>{
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+    console.log("creating user")
+    const user =  await prisma.user.create({
+      data:{
+        email : body.email,
+        password : body.password
+      }
+    });
+
+    const payload ={
+      id: user.id,
+      exp: Math.floor(Date.now() / 1000) + 60 * 5, 
+    }
+
+    const token = await sign(payload,c.env.JWT_SECRET)
+  
+
+    return c.json({
+      jwt : "token"
+    })
 })
 
 app.post('/api/v1/signup',async (c) =>{  
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
-  const body = await c.req.json();
-  const user = await prisma.user.findUnique({
-    where :{
-      email : body.email,
-      password: body.password
-    }
-  })
-
-  if(!user){
-    return c.json({
-      msg : "You don't have an account",
+    const body = await c.req.json();
+    const user = await prisma.user.findUnique({
+      where :{
+        email : body.email,
+        password: body.password
+      }
     })
-  }
 
-  const payload ={
-    id: user.id, 
-  }
+    if(!user){
+      return c.json({
+        msg : "You don't have an account",
+      })
+    }
 
-  const token = await sign(payload,c.env.JWT_SECRET)
- 
-  return c.json({
-    jwt: token,
-    msg: "logged in successfully",
-  })
+    const payload ={
+      id: user.id, 
+    }
+
+    const token = await sign(payload,c.env.JWT_SECRET)
+  
+    return c.json({
+      jwt: token,
+      msg: "logged in successfully",
+    })
 
 })
+
 app.post('/api/vi/blog/:id', (c) =>{
-  const id = c.req.param('id');
-  console.log(id);
-  return c.text("get blog route")
+    const id = c.req.param('id');
+    console.log(id);
+    return c.text("get blog route")
 })
-app.put('/api/vi/blog', (c) =>{
-  return c.text("get all blod");
+
+app.put('/api/v1/blog', (c) =>{
+    return c.text("get all blod");
 })
+
 
 app.post('/api/vi/blog/bulk', (c) =>{
-  return c.text("get all bolck in bulk")
+    return c.text("get all bolck in bulk")
 })
 
 export default app
